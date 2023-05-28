ded220e85ac8ec77fa41c0e00a985d3b6a92ff92
Adrian Domański <adrian.domanski@prographers.com>
2023-05-28 11:50:32 +0200

    feat: add auth with Clerk

---
+++ src/middleware.ts
@@ -0,0 +1,8 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware();
+
+// Protected routes will be matched by this matcher e.g. /dashboard
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/(api|trpc)(.*)"],
+};
